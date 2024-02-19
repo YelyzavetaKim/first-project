39e71a845f935f06624a8d526b12b68088a91c62
YelyzavetaKim <160435711+YelyzavetaKim@users.noreply.github.com>
2024-02-19 17:12:37 +0200

    Update LW1.py

--- LW1.py
+++ LW1.py
@@ -1,3 +1,4 @@
+#Task1
 a = int(input("Введіть a: "))
 while a <= 0:
     a = int(input("Введіть ще раз a (повинно бути додатнім): "))
@@ -11,3 +12,30 @@ elif a == b:
 else:
     r = 3 * a / b + 1
 print("Результат обчислення виразу:", r)
+
+#Task2
+def fibonacci(n):
+    fib = [0, 1]
+    for i in range(2, n):
+        fib.append(fib[i-1] + fib[i-2])
+    return fib
+
+start = 5
+end = 25
+
+fib_series = fibonacci(end)
+print("Ряд Фібоначчі від", start, "до", end, "члену:")
+print(fib_series[start:end])
+
+fib_count = end - start
+print("Кількість членів ряду Фібоначчі:", fib_count)
+
+#Task3
+N = int(input("Введіть ціле число N (1<N<9): "))
+while N <= 1 or N >= 9:
+    N = int(input("Введіть ще раз ціле число N (1<N<9): "))
+for i in range(1, N + 1):
+    for j in range(i, 0, -1):
+        print(j, end=" ")
+    print()
+    
